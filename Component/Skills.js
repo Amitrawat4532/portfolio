fb3572f1cd3483007c7ee121c9aeaa366d95ea5d
amitrawat <amitrawat4532@gmail.com>
2022-10-22 16:52:48 +0530

    fix bug

--- Component/Skills.js
+++ Component/Skills.js
@@ -11,38 +11,58 @@ const Skills = () => {
         gap="30px"
       >
         <Flex height="50px" width="100%" justifyContent="center">
-          <Text color="white" fontSize="30px" fontWeight="400">
-            Skills
+          <Text color="white" fontSize="35px" fontWeight="600">
+            SKILLS
           </Text>
         </Flex>
-        <Box w="100%" h="100%">
-          <Stack spacing={5} w="100%" h="100%">
+        <Flex w="100%" h="100%" justifyContent="center">
+          <Stack spacing={5} w="50%" h="100%">
             <Text color="yellow" textAlign="center">
               HTML
             </Text>
-            <Progress colorScheme="orange" height="20px" value={80} />
+            <Progress
+              colorScheme="orange"
+              height={["10px", "15px", "20px", "20px"]}
+              value={80}
+            />
 
             <Text color="yellow" textAlign="center">
               CSS
             </Text>
 
-            <Progress colorScheme="orange" height="20px" value={30} />
+            <Progress
+              colorScheme="orange"
+              height={["10px", "15px", "20px", "20px"]}
+              value={30}
+            />
             <Text color="yellow" textAlign="center">
               JavaScript
             </Text>
-            <Progress colorScheme="orange" height="20px" value={20} />
+            <Progress
+              colorScheme="orange"
+              height={["10px", "15px", "20px", "20px"]}
+              value={20}
+            />
             <Text color="yellow" textAlign="center">
               React.js
             </Text>
-            <Progress colorScheme="orange" height="20px" value={90} />
+            <Progress
+              colorScheme="orange"
+              height={["10px", "15px", "20px", "20px"]}
+              value={90}
+            />
 
             <Text color="yellow" textAlign="center">
               Next.js
             </Text>
 
-            <Progress colorScheme="orange" height="20px" value={20} />
+            <Progress
+              colorScheme="orange"
+              height={["10px", "15px", "20px", "20px"]}
+              value={20}
+            />
           </Stack>
-        </Box>
+        </Flex>
       </Flex>
     </>
   );
